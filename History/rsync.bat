4e74f23fd507b341725f898e5b2e44add9683c5c
Michael Oborne <mich146@hotmail.com>
2019-11-26 15:45:06 +0800

    AP 4.0.1 AR 3.5.2 AC 3.6.11 AT 1.1

--- History/rsync.bat
+++ History/rsync.bat
@@ -1,12 +1,12 @@
 
-start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@mega.ardupilot.org:binaries/Copter/stable/ "/cygdrive/c/ProgramData/Mission Planner/History/Copter/stable"
+start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Copter/stable/ "Copter/stable"
 
-start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@mega.ardupilot.org:binaries/Plane/stable/ "/cygdrive/c/ProgramData/Mission Planner/History/Plane/stable"
+start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Plane/stable/ "Plane/stable"
 
-start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@mega.ardupilot.org:binaries/Rover/stable/ "/cygdrive/c/ProgramData/Mission Planner/History/Rover/stable"
+start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Rover/stable/ "Rover/stable"
 
-start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@mega.ardupilot.org:binaries/Sub/stable/ "/cygdrive/c/ProgramData/Mission Planner/History/Sub/stable"
+start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Sub/stable/ "Sub/stable"
 
-start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@mega.ardupilot.org:binaries/AntennaTracker/stable/ "/cygdrive/c/ProgramData/Mission Planner/History/AntennaTracker/stable"
+start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/AntennaTracker/stable/ "AntennaTracker/stable"
 
 pause
