7003e370dffdfbb9d6bc3813a46729a1b8a8fd70
Michael Oborne <mich146@hotmail.com>
2020-03-07 08:54:23 +0800

    ArduCopter V4.0.0 ArduPlane V4.0.2

--- History/rsync.bat
+++ History/rsync.bat
@@ -1,7 +1,7 @@
 
-start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Copter/stable/ "Copter/stable"
+start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Copter/stable-4.0.0/ "Copter/stable"
 
-start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Plane/stable/ "Plane/stable"
+start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Plane/stable-4.0.2/ "Plane/stable"
 
 start c:\cygwin\bin\rsync.exe -Pv -r -e '/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /cygdrive/c/Users/michael/sitl' michael@firmware.ardupilot.org:/storage/autotest/APM/buildlogs/binaries/Rover/stable/ "Rover/stable"
 
